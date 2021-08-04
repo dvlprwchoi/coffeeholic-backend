e5f6561a42852590c0471faa21bb7f9e9bc4dd06
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-08-04 18:15:01 -0500

    Add coffeeholic views

--- coffeeholic/views.py
+++ coffeeholic/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import CoffeeShop
+from .serializers import CoffeeShopSerializer
+from coffeeholic.permissions import IsOwnerReadOnly
 
-# Create your views here.
+
+class CoffeeShopList(generics.ListCreateAPIView):
+    queryset = CoffeeShop.objects.all()
+    serializers_class = CoffeeShopSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CoffeeShopDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CoffeeShop.objects.all()
+    serializers_class = CoffeeShopSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
